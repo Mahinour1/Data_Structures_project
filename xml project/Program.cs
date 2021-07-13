87f0ef5caa7406fdf50bc4602e0e551195dc5092
noura <1701595@eng.asu.edu.eg>
2021-07-13 17:37:30 +0200

    error correction

--- xml project/Program.cs
+++ xml project/Program.cs
@@ -26,21 +26,17 @@ namespace xml_project
 			Stack<string> s =new Stack<string>();
 			int index = 0;
 			string csub = c[0];
-			string prevstring = null; int d; string x;
+			int d;
 			while (index < c.Length)
 			{
 				int iter = csub.IndexOf('<');
 				if (iter == -1 && csub.Length != 0) // line is only words
 				{
 					flag = 1;
-					prevstring = prevstring + csub; // save these words in prevstring
+					
 					index++; // go to next line
 					if (index < c.Length) // check that file isn't finished
 						csub = c[index];
-
-
-
-
 				}
 				else if (iter != -1)
 				{
@@ -69,4 +65,79 @@ namespace xml_project
 							if (index < c.Length)
 								csub = c[index];
 						}
-						
+						else // may be there are spaces only after closing tag => we don't need them
+						{
+							d = 0; // flag
+							for (int i = 0; i < csub.Length; i++)
+							{
+								if (csub[i] != ' ')
+								{
+									d = 1;
+									break;
+								}
+							}
+							if (d == 0) // all characters are spaces
+							{
+								index++; // go to next line
+								if (index < c.Length)
+									csub = c[index];
+							}
+						}
+					}
+					else if (csub[iter + 1] != '/') // opening tag
+					{
+						if (flag != 1)
+						{
+							int close = csub.IndexOf('>');
+							int space = csub.IndexOf(' ');
+							int dash = csub.IndexOf('/');
+							int open = csub.IndexOf('<');
+							if (dash == -1 || dash > close) // not opening and closing at the same time
+							{
+								if (space == -1 || (space > close || (space < close && space < open))) // there is no value <tag>
+									name = csub.Substring(iter + 1, close - iter - 1); // set child name
+								else // there is value <tag ..>
+								{
+									name = csub.Substring(iter + 1, space - iter - 1); // set child name
+								}
+								s.Push(name);
+							}
+							csub = csub.Substring(close + 1);
+							if (csub.Length == 0) // there's nothing after tag
+							{
+								index++; // go to next line
+								if (index < c.Length)
+									csub = c[index];
+							}
+							else // may be there are spaces only after tag => we don't need them
+							{
+								d = 0; // flag
+								for (int i = 0; i < csub.Length; i++)
+								{
+									if (csub[i] != ' ')
+									{
+										d = 1;
+										break;
+									}
+								}
+								if (d == 0) // all characters are spaces
+								{
+									index++; // go to next line
+									if (index < c.Length)
+										csub = c[index];
+								}
+							}
+						}
+						else if (flag == 1)
+						{
+							error[index - 1] = 1;
+							copy[index - 1] += "</" + s.Pop() + ">";
+							flag = 0;
+						}
+
+					}
+				}
+			}
+		}
+	}
+}
