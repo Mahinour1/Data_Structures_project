dbfa3eb76b1b78dc59fe9de37a61f8372ac88834
Mahinour Amr <mahinouramr1998@gmail.com>
2021-07-14 22:50:06 +0200

    Heapnode, freq function

---
+++ xml project/Compression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xml_project
+{
+    class HeapNode
+    {
+        public char Data { get; set; }
+        public uint Freq { get; set; }
+        public HeapNode Left { get; set; }
+        public HeapNode Right { get; set; }
+        
+    }
+    
+}
--- xml project/Program.cs
+++ xml project/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace xml_project
 {
@@ -6,7 +8,59 @@ namespace xml_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			string[] lines = System.IO.File.ReadAllLines(@"D:\trial.txt");
+			Queue<HeapNode> queue = new Queue<HeapNode>();
+			Frequency(lines, queue);
+			Queue<HeapNode> copy = new Queue<HeapNode>();
+			copy = queue;
+			while (copy.Count != 0)
+			{
+				Console.WriteLine(copy.Peek().Data + " " + copy.Peek().Freq);
+				copy.Dequeue();
+			}
+			
+		}
+		
+        static void Frequency(string[] input, Queue<HeapNode> q)
+        {
+			string[] copy = new string[input.Length];
+			for (int i = 0; i < input.Length; i++)
+			{
+				copy[i] = input[i];
+			}
+            for (int i = 0; i < copy.Length; i++)
+            {
+                for (int j = 0; j < copy[i].Length; j++)
+                {
+                    char compare = (copy[i])[j];
+                    uint freq = 0;
+                    for (int k = 0; k < copy.Length; k++)
+                    {
+                        for (int f = 0; f < copy[k].Length; f++)
+                        {
+							if ( compare !='0' && (copy[k])[f] != '0')
+							{
+								if ((copy[k])[f] == compare)
+								{
+									freq++;
+									string n = copy[k].Substring(0, f) + "0" + copy[k].Substring(f + 1);
+									copy[k] = n;
+								}
+							}
+                           
+                        }
+                    }
+					if (compare != '0')
+					{
+						HeapNode node = new HeapNode();
+						node.Data = compare;
+						node.Freq = freq;
+						q.Enqueue(node);
+					}
+					
+                }
+            }
         }
-    }
+       
+	}
 }
