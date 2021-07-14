1d151d05b039e83fc756326713527d8bc568fd53
nehalyasser <64105565+nehalyasser@users.noreply.github.com>
2021-07-15 01:26:17 +0200

    formatting with twins

--- xml project/Program.cs
+++ xml project/Program.cs
@@ -8,14 +8,14 @@ namespace xml_project
         {
             Console.WriteLine("Hello World!");
         }
-        static void start(node n, int has_twin = 0, int twin_write = 0)
+        static void start(node n, int m, int has_twin = 0, int twin_write = 0)
         {
             Console.WriteLine("{");
-            convert(n, has_twin, twin_write);
+            convert(n, m, has_twin, twin_write);
             Console.WriteLine("\n}");
         }
 
-        static void convert(node n, int has_twin, int twin_write)
+        static void convert(node n, int m, int has_twin, int twin_write)
         {
             if ((n.children.Count == 0) && (has_twin == 0)) //has data //base case
             {
@@ -52,12 +52,27 @@ namespace xml_project
                     Console.Write("}");
                     //Console.Write("\"" + n.name + "\":{\"@" + v_name + "\":\"" + v_value + "\",\"#text\":\"" + n.data);
                 }
-
+                else if ((n.type == 1) && (n.value != null) && (n.data == null))
+                {
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    Console.WriteLine("\"" + n.name + "\":{");
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    //Console.WriteLine("\"#text\":\"" + n.data + "\"");
+                    Console.Write("}");
+                    //Console.Write("\"" + n.name + "\":{\"@" + v_name + "\":\"" + v_value + "\",\"#text\":\"" + n.data);
+                }
                 return;
             }
 
             else if ((n.children.Count == 0) && (has_twin == 1)) //has data //base case
             {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                }
                 //case 1 in table
                 if ((n.type == 0) && (n.value == null))
                     Console.Write("null");
@@ -86,10 +101,47 @@ namespace xml_project
                     v_name = n.value.Substring(0, eq);
                     v_value = n.value.Substring(eq + 1);
                     Console.WriteLine("{");
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                    }
                     Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                    }
                     Console.WriteLine("\"#text\":\"" + n.data + "\"");
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
                     Console.Write("}");
                     //Console.Write("\"" + n.name + "\":{\"@" + v_name + "\":\"" + v_value + "\",\"#text\":\"" + n.data);
+                }
+                else if ((n.type == 1) && (n.value != null) && (n.data == null))
+                {
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    Console.WriteLine("{");
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    //for (int j = 0; j < m + 1; j++)
+                    //{
+                    //    Console.Write(" ");
+                    //}
+
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    Console.Write("}");
                 }
 
                 return;
@@ -109,31 +161,7 @@ namespace xml_project
                 if (twin_write == 0)
                     Console.Write("\"" + n.name + "\":");
                 Console.Write("{");
-                for (int i = 0; i < n.children.Count; i++)
-                {
-                    Console.Write("\n");
-                    if (n.value != null)
-                    {
-                        string v_name, v_value;
-                        int eq = n.value.IndexOf("=");
-                        v_name = n.value.Substring(0, eq);
-                        v_value = n.value.Substring(eq + 1);
-                        Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
-                    }
-                    convert(n.children[i], has_twin, twin_write);
-                    if (i != (n.children.Count - 1))
-                        Console.Write(",");
-                    if (i == (n.children.Count - 1))
-                        Console.Write("\n");
-
-                }
-                Console.Write("}");
-            }
-            //if children are twins
-            else if (has_twin == 1)
-            {
-                if (twin_write == 0)
-                    Console.WriteLine("\"" + n.name + "\":{");
+                Console.Write("\n");
                 if (n.value != null)
                 {
                     string v_name, v_value;
@@ -142,13 +170,9 @@ namespace xml_project
                     v_value = n.value.Substring(eq + 1);
                     Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
                 }
-                Console.Write("\"" + n.children[0].name + "\":[");
-                twin_write = 1;
-
                 for (int i = 0; i < n.children.Count; i++)
                 {
-
-                    Console.Write("\n");
+                    //Console.Write("\n");
                     //if (n.value != null)
                     //{
                     //    string v_name, v_value;
@@ -157,13 +181,73 @@ namespace xml_project
                     //    v_value = n.value.Substring(eq + 1);
                     //    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
                     //}
-                    convert(n.children[i], has_twin, twin_write);
+                    m++;
+                    convert(n.children[i], m, has_twin, twin_write);
+                    m--;
+                    if (i != (n.children.Count - 1))
+                        Console.Write(",");
+                    if (i == (n.children.Count - 1))
+                        Console.Write("\n");
+
+                }
+
+                Console.Write("}");
+            }
+            //if children are twins
+            else if (has_twin == 1)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                }
+                if (twin_write == 0)
+                    Console.WriteLine("\"" + n.name + "\":{");
+                else
+                    Console.WriteLine("{");
+                if (n.value != null)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                }
+                m++;
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("\"" + n.children[0].name + "\":[");
+
+                twin_write = 1;
+
+                for (int i = 0; i < n.children.Count; i++)
+                {
+
+                    Console.Write("\n");
+
+                    m++;
+                    convert(n.children[i], m, has_twin, twin_write);
+                    m--;
                     if (i != (n.children.Count - 1))
                         Console.Write(",");
                     if (i == (n.children.Count - 1))
                         Console.Write("\n");
                 }
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                }
+                m--;
                 Console.WriteLine("]");
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                }
                 Console.Write("}");
             }
         }
