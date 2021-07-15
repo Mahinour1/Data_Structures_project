be7ec97d65d8f24b36238c03b7ae9269a7b5a4ce
noura <1701595@eng.asu.edu.eg>
2021-07-15 15:36:07 +0200

    adding json with formatting

--- xml project/Form1.Designer.cs
+++ xml project/Form1.Designer.cs
@@ -40,12 +40,14 @@ namespace WindowsFormsApp1
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Kristen ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(393, 12);
+            this.button1.Location = new System.Drawing.Point(18, 9);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(470, 72);
             this.button1.TabIndex = 0;
@@ -56,21 +58,22 @@ namespace WindowsFormsApp1
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 102);
+            this.textBox1.Location = new System.Drawing.Point(12, 140);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(495, 523);
+            this.textBox1.Size = new System.Drawing.Size(589, 690);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(793, 102);
+            this.textBox2.Location = new System.Drawing.Point(856, 140);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(593, 523);
+            this.textBox2.Size = new System.Drawing.Size(618, 724);
             this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // ofd1
             // 
@@ -80,7 +83,7 @@ namespace WindowsFormsApp1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Kristen ITC", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 51);
+            this.label1.Location = new System.Drawing.Point(12, 93);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(203, 33);
             this.label1.TabIndex = 3;
@@ -92,7 +95,7 @@ namespace WindowsFormsApp1
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Kristen ITC", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.label2.Location = new System.Drawing.Point(1002, 51);
+            this.label2.Location = new System.Drawing.Point(876, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 33);
             this.label2.TabIndex = 3;
@@ -103,7 +106,7 @@ namespace WindowsFormsApp1
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Kristen ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(534, 149);
+            this.button2.Location = new System.Drawing.Point(620, 201);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(204, 81);
             this.button2.TabIndex = 4;
@@ -114,7 +117,7 @@ namespace WindowsFormsApp1
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Kristen ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(534, 244);
+            this.button3.Location = new System.Drawing.Point(620, 444);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(204, 72);
             this.button3.TabIndex = 5;
@@ -125,7 +128,7 @@ namespace WindowsFormsApp1
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Kristen ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(534, 433);
+            this.button4.Location = new System.Drawing.Point(620, 318);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(204, 81);
             this.button4.TabIndex = 6;
@@ -136,9 +139,9 @@ namespace WindowsFormsApp1
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Kristen ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(1140, 37);
+            this.button5.Location = new System.Drawing.Point(1251, 79);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(246, 47);
+            this.button5.Size = new System.Drawing.Size(204, 47);
             this.button5.TabIndex = 7;
             this.button5.Text = "Save to PC";
             this.button5.UseVisualStyleBackColor = true;
@@ -147,7 +150,7 @@ namespace WindowsFormsApp1
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Kristen ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(534, 337);
+            this.button6.Location = new System.Drawing.Point(620, 548);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(204, 57);
             this.button6.TabIndex = 8;
@@ -155,11 +158,35 @@ namespace WindowsFormsApp1
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Kristen ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(620, 645);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(204, 46);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Compress";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Kristen ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(620, 727);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(204, 46);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Decompress";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1408, 610);
+            this.ClientSize = new System.Drawing.Size(1510, 842);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -173,6 +200,7 @@ namespace WindowsFormsApp1
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -191,6 +219,8 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- xml project/Program.cs
+++ xml project/Program.cs
@@ -126,5 +126,402 @@ namespace WindowsFormsApp1
 			}
 			return decoded;
 		}
-	}
+        public void start(node n, ref string json,int m=1,int has_twin = 0, int twin_write = 0)
+        {
+           
+            Console.WriteLine("{");
+            json += "{" + Environment.NewLine;
+            convert(n,ref json, m, has_twin, twin_write);
+            Console.WriteLine("\n}");
+            json += Environment.NewLine + "}" + Environment.NewLine;
+        }
+
+        public void convert(node n, ref string json, int m, int has_twin, int twin_write)
+        {
+            if ((n.children.Count == 0) && (has_twin == 0)) //has data //base case
+            {
+                //case 1 in table
+                if ((n.type == 0) && (n.value == null))
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.Write("\"" + n.name + "\":null");
+                    json += "\"" + n.name + "\":null";
+                }
+
+                //case 3 in table
+                else if ((n.type == 0) && (n.value != null))
+                {
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"" + n.name + "\":{");
+                    json += "\"" + n.name + "\":{" + Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value);
+                    json += "\"@" + v_name + "\":" + v_value + Environment.NewLine;
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.Write("}");
+                    json += "}";
+                    //Console.Write("\"" + n.name+"\":{\"@"+v_name+"\":\""+v_value+"\"}");
+                }
+                //case 2 in table
+                else if ((n.type == 1) && (n.value == null) && (n.data != null))
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.Write("\"" + n.name + "\":\"" + n.data + "\"");
+                    json += "\"" + n.name + "\":\"" + n.data + "\"";
+                }
+
+                //case 4 in table
+                else if ((n.type == 1) && (n.value != null) && (n.data != null))
+                {
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"" + n.name + "\":{");
+                    json += "\"" + n.name + "\":{" + Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    json += "\"@" + v_name + "\":" + v_value + "," + Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"#text\":\"" + n.data + "\"");
+                    json += "\"#text\":\"" + n.data + "\"" + Environment.NewLine;
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.Write("}");
+                    json += "}";
+                    //Console.Write("\"" + n.name + "\":{\"@" + v_name + "\":\"" + v_value + "\",\"#text\":\"" + n.data);
+                }
+                else if ((n.type == 1) && (n.value != null) && (n.data == null))
+                {
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"" + n.name + "\":{");
+                    json += "\"" + n.name + "\":{" + Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    json += "\"@" + v_name + "\":" + v_value + "," + Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.Write("}");
+                    json += "}";
+                    //Console.Write("\"" + n.name + "\":{\"@" + v_name + "\":\"" + v_value + "\",\"#text\":\"" + n.data);
+                }
+                return;
+            }
+
+            else if ((n.children.Count == 0) && (has_twin == 1)) //has data //base case
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                    json += " ";
+                }
+                //case 1 in table
+                if ((n.type == 0) && (n.value == null))
+                {
+                    Console.Write("null");
+                    json += "null";
+                }
+
+                //case 3 in table
+                else if ((n.type == 0) && (n.value != null))
+                {
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    Console.WriteLine("{");
+                    json += "{" + Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value);
+                    json += "\"@" + v_name + "\":" + v_value + Environment.NewLine;
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.Write("}");
+                    json += "}";
+                    //Console.Write("\"" + n.name+"\":{\"@"+v_name+"\":\""+v_value+"\"}");
+                }
+                //case 2 in table
+                else if ((n.type == 1) && (n.value == null) && (n.data != null))
+                { Console.Write("\"" + n.data + "\"");
+                    json += "\"" + n.data + "\"";
+                }
+                //case 4 in table
+                else if ((n.type == 1) && (n.value != null) && (n.data != null))
+                {
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    Console.WriteLine("{");
+                    json += "{" + Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    json += "\"@" + v_name + "\":" + v_value + "," + Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"#text\":\"" + n.data + "\"");
+                    json += "\"#text\":\"" + n.data + "\""+Environment.NewLine;
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+
+                    Console.Write("}");
+                    json += "}";
+                    //Console.Write("\"" + n.name + "\":{\"@" + v_name + "\":\"" + v_value + "\",\"#text\":\"" + n.data);
+                }
+
+                else if ((n.type == 1) && (n.value != null) && (n.data == null))
+                {
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    Console.WriteLine("{");
+                    json += "{" + Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    json += "\"@" + v_name + "\":" + v_value + "," + Environment.NewLine;
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+
+                    Console.Write("}");
+                    json += "}";
+                }
+
+                return;
+            }
+            // if has children we should decide first if they are twins
+            if ((n.children.Count > 1) && (n.children[0].name == n.children[1].name))
+                has_twin = 1;
+            else
+            {
+                has_twin = 0;
+                //twin_write = 0;
+            }
+
+            //if children are not twins
+            if ((has_twin == 0))
+            {
+
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                    json += " ";
+                }
+                if (twin_write == 0)
+                {
+                    Console.Write("\"" + n.name + "\":");
+                    json += "\"" + n.name + "\":";
+                }
+                Console.Write("{");
+                json += "{";
+
+                if (n.value != null)
+                {
+                    Console.Write("\n");
+                    json += Environment.NewLine;
+                    for (int j = 0; j < m + 1; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    json += "\"@" + v_name + "\":" + v_value + "," + Environment.NewLine;
+                }
+                for (int i = 0; i < n.children.Count; i++)
+                {
+                    Console.Write("\n");
+                    json += Environment.NewLine;
+                    //if (n.value != null)
+                    //{
+                    //    string v_name, v_value;
+                    //    int eq = n.value.IndexOf("=");
+                    //    v_name = n.value.Substring(0, eq);
+                    //    v_value = n.value.Substring(eq + 1);
+                    //    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    //}
+                    m++;
+
+                    convert(n.children[i], ref json, m, has_twin, twin_write);
+                    m--;
+                    if (i != (n.children.Count - 1))
+                    {
+                        Console.Write(",");
+                        json += ",";
+                    }
+                    if (i == (n.children.Count - 1))
+                    {
+                        Console.Write("\n");
+                        json += Environment.NewLine;
+                    }
+                }
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                    json += " ";
+                }
+                Console.Write("}");
+                json += "}";
+            }
+            //if children are twins
+            else if (has_twin == 1)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                    json += " ";
+                }
+                if (twin_write == 0)
+                {
+                    Console.WriteLine("\"" + n.name + "\":{");
+                    json += "\"" + n.name + "\":{" + Environment.NewLine;
+                }
+
+                else
+                {   Console.WriteLine("{");
+                json += "{" + Environment.NewLine;
+                 }
+                if (n.value != null)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(" ");
+                        json += " ";
+                    }
+                    string v_name, v_value;
+                    int eq = n.value.IndexOf("=");
+                    v_name = n.value.Substring(0, eq);
+                    v_value = n.value.Substring(eq + 1);
+                    Console.WriteLine("\"@" + v_name + "\":" + v_value + ",");
+                    json += "\"@" + v_name + "\":" + v_value + "," + Environment.NewLine;
+                }
+                m++;
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                    json += " ";
+                }
+                Console.Write("\"" + n.children[0].name + "\":[");
+                json += "\"" + n.children[0].name + "\":[";
+
+                twin_write = 1;
+
+                for (int i = 0; i < n.children.Count; i++)
+                {
+
+                    Console.Write("\n");
+                    json += Environment.NewLine;
+
+                    m++;
+                    convert(n.children[i], ref json, m, has_twin, twin_write);
+                    m--;
+                    if (i != (n.children.Count - 1))
+                    { Console.Write(",");
+                    json += ",";
+                    }
+                    if (i == (n.children.Count - 1))
+                    { Console.Write("\n");
+                        json += Environment.NewLine;
+                            }
+                }
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                    json += " ";
+                }
+                m--;
+                Console.WriteLine("]");
+                json += "]" + Environment.NewLine;
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                    json += " ";
+                }
+                Console.Write("}");
+                json += "}";
+            }
+        }
+    }
 }
